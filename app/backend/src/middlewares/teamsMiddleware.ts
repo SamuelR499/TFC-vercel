01ee9910ff41e7a4ac2a25b8e94e57097c40eef6
Samuelr499 <samuelribeiro2@hotmail.com>
2022-10-26 20:15:47 -0300

    feat: teamsMiddleware

--- app/backend/src/middlewares/teamsMiddleware.ts
+++ app/backend/src/middlewares/teamsMiddleware.ts
@@ -1,12 +1,28 @@
+import * as jwt from 'jsonwebtoken';
+import 'dotenv/config';
 import { Request, Response, NextFunction } from 'express';
 import HttpException from './HTTPexception';
 
-const teamsMiddleware = async (req: Request, _res: Response, next: NextFunction) => {
+const teamsMiddleware = (req: Request, _res: Response, next: NextFunction) => {
+  const secret = process.env.JWT_SECRET || ('jwt_secret' as jwt.Secret);
+  const token = req.headers.authorization as string;
   const { awayTeam, homeTeam } = req.body;
 
-  if (awayTeam === homeTeam) {
+  if (!token) {
+    throw new HttpException(401, 'Token not found');
+  }
+  if (!homeTeam || !awayTeam) {
+    throw new HttpException(404, 'There is no team with such id!');
+  }
+  if (homeTeam === awayTeam) {
     throw new HttpException(422, 'It is not possible to create a match with two equal teams');
   }
+  try {
+    jwt.verify(token, secret);
+  } catch (error) {
+    throw new HttpException(401, 'Token must be a valid token');
+  }
+
   next();
 };
 
