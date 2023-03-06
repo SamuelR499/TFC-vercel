1a00e24e9d07566aa025d3b0c5f7f77ab82352e0
Samuel Ribeiro <samuelribeiro2@hotmail.com>
2023-03-06 13:47:11 -0300

    fix: refatora baseURL

--- app/frontend/src/services/requests.js
+++ app/frontend/src/services/requests.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://tfc-vercel-production.up.railway.app/',
+  baseURL: 'https://tfc-vercel-production.up.railway.app/leaderboard/',
 });
 
 export const setToken = (token) => {
